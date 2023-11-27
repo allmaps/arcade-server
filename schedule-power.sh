c779894d69eba93a4cd23c5248dd66dd3ca99f00
Bert Spaan <bert.spaan@gmail.com>
2023-11-27 12:44:35 +0100

    fixed set schedule

--- schedule-power.sh
+++ schedule-power.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo pmset repeat wakeorpoweron MTWRF 07:45:00
+sudo pmset repeat wakeorpoweron MTWRF 07:45:00 shutdown MTWRF 23:59:59
 
-sudo pmset repeat shutdown MTWRF 23:59:59
+pmset -g sched
