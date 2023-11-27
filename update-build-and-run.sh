2a163c672172dc7ab4c0323c6846b2f6d00846af
Bert Spaan <bert.spaan@gmail.com>
2023-11-27 15:46:45 +0100

    added echo

--- update-build-and-run.sh
+++ update-build-and-run.sh
@@ -2,23 +2,31 @@
 
 cd "$(dirname "$0")"
 
+echo "Updating and building Allmaps Arcade"
+
 ./update-and-build.sh || echo "Failed to build arcade"
+
+echo "Starting Docker"
 
 open -a Docker
 
 while (! docker stats --no-stream ); do
   # Docker takes a few seconds to initialize
-  echo "Waiting for Docker to launch..."
+  echo "  Waiting for Docker to launch..."
   sleep 1
 done
+
+echo "Starting Caddy server"
 
 docker-compose up --detach --remove-orphans
 
 while ! curl http://localhost/
 do
-  echo "Waiting for Caddy server to launch..."
+  echo "  Waiting for Caddy server to launch..."
   sleep 1
 done
+
+echo "Starting Google Chrome"
 
 open -a "Google Chrome" --args --start-maximized \
   --start-fullscreen --kiosk --app=http://localhost/
