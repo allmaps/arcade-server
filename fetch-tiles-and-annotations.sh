afd4be257d2d0c8120a5d75f3784f57e7ea5a29b
Bert Spaan <bert.spaan@gmail.com>
2023-11-30 10:24:51 +0100

    added echo

--- fetch-tiles-and-annotations.sh
+++ fetch-tiles-and-annotations.sh
@@ -2,9 +2,12 @@
 
 cd "$(dirname "$0")"
 
+echo "Stashing and pulling arcade-server"
 git stash
 git pull || echo "Failed to pull arcade-server"
 
+echo "Installing arcade-server dependencies"
 pnpm install
 
+echo "Running fetch-tiles-and-annotations.ts"
 bun fetch-tiles-and-annotations.ts >> ./files/logs/fetch-tiles-and-annotations.log
--- update-and-build.sh
+++ update-and-build.sh
@@ -4,10 +4,15 @@ cd "$(dirname "$0")"
 
 cd ../arcade
 
+echo "Stashing and pulling Allmaps Arcade"
 git stash
 git pull
 
+echo "Installing Allmaps Arcade dependencies"
 pnpm install
+
+echo "Building Allmaps Arcade"
 pnpm run build
 
+echo "Copying build to arcade-server"
 cp -r ./build/* ../arcade-server/files
