e579729e26a7b20c876dfd9fbf23612dff2ad522
Bert Spaan <bert.spaan@gmail.com>
2023-12-05 17:15:30 +0100

    added chmod

--- install-launch-agents.sh
+++ install-launch-agents.sh
@@ -2,4 +2,6 @@
 
 cp ./update-build-and-run.plist ~/Library/LaunchAgents/
 
+# TODO: use enable?
 launchctl load -w ~/Library/LaunchAgents/update-build-and-run.plist
+chmod 0600 ~/Library/LaunchAgents/update-build-and-run.plist
